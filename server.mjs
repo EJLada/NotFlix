16a30d59db9fc4e8b6edd85cb8973fde36d7ca16
EJLada <ejlada@gmail.com>
2022-03-06 15:10:50 -0500

    Finish Create route handlers
    Also added read single-item route handlers for the format
    '/customers/:id'.
    Single-item read is NOT available on contenttypes since they're using a
    concatenated primary key.
    
    EJL

--- server.mjs
+++ server.mjs
@@ -15,6 +15,8 @@ app.get('/', function(req, res) {
     res.send("NotFlix Backend API running.");
 });
 
+const HOME = 'https://notflix_backend.herokuapp.com';
+
 // Database
 import mysql from 'mysql';
 const db = mysql.createPool(process.env.JAWSDB_MARIA_URL);
@@ -46,6 +48,35 @@ function queryBuilder(query, body) {
     ROUTES
 */
 // ------------------------CUSTOMER ROUTES------------------------------------//
+// CREATE new Customer record
+app.post('/customers', function(req, res) {
+    // Check for all necessary data to create record
+    for (const _ in ['firstName', 'lastName', 'email']) {
+        if (!_ in Object.keys(req.body)) {
+            res.status(400);
+            res.send('Bad Request');
+        }
+    }
+
+    let addCustomer = 'INSERT INTO Customers (firstName, lastName, email) ' +
+        'VALUES (' + req.body.firstName + ', ' + req.body.lastName
+        + ', ' + req.body.email + '); SELECT LAST_INSERT_ID();';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(addCustomer, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(201);
+            res.send(HOME + '/customers/' + results);
+        });
+    });
+});
+
 // READ all or selected Customers
 app.get('/customers', function(req, res) {
     // Define query
@@ -72,7 +103,57 @@ app.get('/customers', function(req, res) {
     });
 });
 
+// READ an individual Customer record
+app.get('/customers/:id', function(req, res) {
+    // Define query
+    let getCustomer = 'SELECT customerID, firstName, lastName, email FROM ' +
+        'Customers WHERE customerID=' + req.params.id + ';';
+
+    // Get data
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(getCustomer, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.send({customer: results});
+        });
+    });
+});
+
 // --------------------------SERIES ROUTES------------------------------------//
+// CREATE new Series record
+app.post('/series', function(req, res) {
+    // Check for all necessary data to create record
+    for (const _ in ['seriesTitle', 'contentRating']) {
+        if (!_ in Object.keys(req.body)) {
+            res.status(400);
+            res.send('Bad Request');
+        }
+    }
+
+    let addSeries = 'INSERT INTO Series (title, contentRating) ' +
+        'VALUES (' + req.body.seriesTitle
+        + ', ' + req.body.contentRating + '); SELECT LAST_INSERT_ID();';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(addSeries, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(201);
+            res.send(HOME + '/series/' + results);
+        });
+    });
+});
+
 // READ all or selected Series
 app.get('/series', function(req, res) {
     let getSeries = 'SELECT seriesID, title, contentRating FROM Series';
@@ -92,12 +173,66 @@ app.get('/series', function(req, res) {
             instance.release();
             if (err) throw err;
             // Send data
+            res.status(200);
+            res.send({series: results});
+        });
+    });
+});
+
+// READ an individual Series record
+app.get('/series/:id', function(req, res) {
+    let getSeries = 'SELECT seriesID, title, contentRating FROM Series ' +
+        'WHERE seriesID=' + req.params.id + ';';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(getSeries, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(200);
             res.send({series: results});
         });
     });
 });
 
 // --------------------------EPISODE ROUTES-----------------------------------//
+// CREATE new Episode record
+app.post('/episodes', function(req, res) {
+    // Check for all necessary data to create record
+    for (const _ in ['seriesID', 'episodeTitle', 'releaseDate',
+        'prevEpisode', 'nextEpisode', 'fileSource']) {
+        if (!_ in Object.keys(req.body)) {
+            res.status(400);
+            res.send('Bad Request');
+        }
+    }
+
+    let addEpisode = 'INSERT INTO Episodes (seriesID, episodeTitle, releaseDate, ' +
+        'previousEpisode, nextEpisode, fileSource) ' +
+        'VALUES (' + req.body.seriesID + ', ' + req.body.episodeTitle
+        + ', ' + req.body.releaseDate + ', ' + req.body.prevEpisode
+        + ', ' + req.body.nextEpisode + ', ' + req.body.fileSource
+        + '); SELECT LAST_INSERT_ID();';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(addEpisode, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(201);
+            res.send(HOME + '/episodes/' + results);
+        });
+    });
+});
+
 // READ all or selected Episodes
 app.get('/episodes', function(req, res) {
     let getEpisodes = 'SELECT episodeID, seriesID, episodeTitle, releaseDate, ' +
@@ -118,12 +253,62 @@ app.get('/episodes', function(req, res) {
             instance.release();
             if (err) throw err;
             // Send data
+            res.status(200);
             res.send({episodes: results});
         });
     });
 });
 
+// READ an individual Episode record
+app.get('/episodes/:id', function(req, res) {
+    let getEpisode = 'SELECT episodeID, seriesID, episodeTitle, releaseDate, ' +
+        'previousEpisode, nextEpisode, fileSource FROM Series WHERE episodeID='
+        + req.params.id + ';';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(getEpisode, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(200);
+            res.send({episode: results});
+        });
+    });
+});
+
 // ------------------------------GENRE ROUTES---------------------------------//
+// CREATE new Genre record
+app.post('/genres', function(req, res) {
+    // Check for all necessary data to create record
+    for (const _ in ['genreName']) {
+        if (!_ in Object.keys(req.body)) {
+            res.status(400);
+            res.send('Bad Request');
+        }
+    }
+
+    let addGenre = 'INSERT INTO Genres (genreName) VALUES (' + req.body.genreName
+        + '); SELECT LAST_INSERT_ID();';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(addGenre, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(201);
+            res.send(HOME + '/genres/' + results);
+        });
+    });
+});
+
 // READ all or selected Genres
 app.get('/genres', function(req, res) {
     let getGenres = 'SELECT genreID, genreName FROM Genres';
@@ -143,13 +328,63 @@ app.get('/genres', function(req, res) {
             instance.release();
             if (err) throw err;
             // Send data
+            res.status(200);
             res.send({genres: results});
+        });
+    });
+});
+
+// READ an individual Genre record
+// READ all or selected Genres
+app.get('/genres/:id', function(req, res) {
+    let getGenre = 'SELECT genreID, genreName FROM Genres WHERE genreID='
+        + req.params.id + ';';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(getGenre, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(200);
+            res.send({genre: results});
         });
     });
 });
 
 
 // ---------------------------SUBSCRIPTION ROUTES-----------------------------//
+// CREATE new Subscription record
+app.post('/subscriptions', function(req, res) {
+    // Check for all necessary data to create record
+    for (const _ in ['customerID', 'seriesID']) {
+        if (!_ in Object.keys(req.body)) {
+            res.status(400);
+            res.send('Bad Request');
+        }
+    }
+
+    let addSubscription = 'INSERT INTO Subscriptions (customerID, seriesID) VALUES ('
+        + req.body.customerID + ', ' + req.body.seriesID + '); SELECT LAST_INSERT_ID();';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(addSubscription, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(201);
+            res.send(HOME + '/subscriptions/' + results);
+        });
+    });
+});
+
 // READ all or selected Subscriptions
 app.get('/subscriptions', function(req, res) {
     let getSubscriptions = 'SELECT subscriptionID, customerID, ' +
@@ -172,15 +407,69 @@ app.get('/subscriptions', function(req, res) {
             instance.release();
             if (err) throw err;
             // Send data
+            res.status(200);
             res.send({subscriptions: results});
         });
     });
 });
 
+// READ an individual Subscription record
+app.get('/subscriptions/:id', function(req, res) {
+    let getSubscription = 'SELECT subscriptionID, customerID, ' +
+        'Customers.firstName as firstName, Customers.lastName as lastName, ' +
+        'seriesID, Series.title as title, dateSubscribed FROM ((Subscriptions ' +
+        'INNER JOIN Customers ON Subscriptions.subscriptionID = Customers.customerID) ' +
+        'INNER JOIN Series ON Subscriptions.seriesID = Series.seriesID) WHERE '
+        + 'subscriptionID=' + req.params.id + ';';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(getSubscription, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(200);
+            res.send({subscription: results});
+        });
+    });
+});
+
 // ---------------------------CONTENT-TYPE ROUTES-----------------------------//
+// CREATE new ContentType record
+app.post('/contenttypes', function(req, res) {
+    // Check for all necessary data to create record
+    for (const _ in ['seriesID', 'genreID']) {
+        if (!_ in Object.keys(req.body)) {
+            res.status(400);
+            res.send('Bad Request');
+        }
+    }
+
+    let addContentType = 'INSERT INTO ContentTypes (seriesID, genreID) VALUES ('
+        + req.body.seriesID + ', ' + req.body.genreID
+        + '); SELECT LAST_INSERT_ID();';
+
+    db.getConnection((err, instance) => {
+        if (err) {
+            res.status(503);
+            res.send('Bad connection to database');
+        }
+        instance.query(addContentType, function(err, results){
+            instance.release();
+            if (err) throw err;
+            // Send data
+            res.status(201);
+            res.send(HOME + '/contenttypes/' + results);
+        });
+    });
+});
+
 // READ all or selected ContentTypes
-app.get('/contentTypes', function(req, res) {
-    let getContentTypes = 'SELECT seriesID, Series.title as title, genreID, Genres.genreName as genreName FROM ' +
+app.get('/contenttypes', function(req, res) {
+    let getContentTypes = 'SELECT seriesID, Series.title as seriesTitle, genreID, Genres.genreName as genreName FROM ' +
         '((ContentTypes INNER JOIN Series ON ContentTypes.seriesID = Series.seriesID) ' +
         'INNER JOIN Genres ON ContentTypes.genreID = Genres.genreID)'
     // Check for search parameters
@@ -198,12 +487,11 @@ app.get('/contentTypes', function(req, res) {
             instance.release();
             if (err) throw err;
             // Send data
+            res.status(200);
             res.send({contentTypes: results});
         });
     });
 });
-
-
 
 
 /*
